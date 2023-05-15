be8d833ba10fa67df7796663e0f53e2634784290
Mateusz Kurek <m.kurek1994@gmail.com>
2023-05-15 21:41:25 +0200

    Navbar UI

---
+++ app/components/Navbar/MenuItem.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+interface MenuItemProps {
+    onClick: () => void;
+    label: string;
+}
+
+export const MenuItem: React.FC<MenuItemProps> = ({ label, onClick }) => {
+  return (
+    <div className="px-4 py-3 hover:bg-neutral-100 transition font-semibold" onClick={onClick}>{label}</div>
+  )
+}
