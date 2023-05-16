97ac48a740eecb6735ef1c8479fce4bb6105da95
Mateusz Kurek <m.kurek1994@gmail.com>
2023-05-16 22:05:52 +0200

    categories added

--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -7,6 +7,7 @@ export const Logo = () => {
   const router = useRouter();
   return (
     <Image
+      onClick={() => router.push("/")}
       alt="Logo"
       className="hidden md:block cursor-pointer"
       height={100}
