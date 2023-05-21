86236534598950b378f72b8c2dcbc5a307da14ce
Mateusz Kurek <m.kurek1994@gmail.com>
2023-05-21 23:47:01 +0200

    add listing details page

---
+++ app/listings/[listingId]/ListingClient.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import React, { useMemo } from "react";
+import { Listing, Reservation } from "@prisma/client";
+
+import { Container } from "@/app/components/Container";
+import { ListingHead } from "@/app/components/Listings/ListingHead";
+import { categories } from "@/app/components/Navbar/Categories";
+
+import { SafeUser } from "@/app/types";
+import { ListingInfo } from "@/app/components/Listings/ListingInfo";
+
+interface ListingClientProps {
+  currentUser?: SafeUser | null;
+  reservations?: Reservation;
+  listing: Listing & {
+    user: SafeUser | null;
+  };
+}
+
+export const ListingClient: React.FC<ListingClientProps> = ({
+  listing,
+  currentUser,
+  reservations,
+}) => {
+  const category = useMemo(() => {
+    return categories.find((item) => item.label === listing.category);
+  }, [listing.category]);
+
+  return (
+    <Container>
+      <div className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col gap-6">
+          <ListingHead
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            locationValue={listing.locationValue}
+            id={listing.id}
+            currentUser={currentUser}
+          />
+          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
